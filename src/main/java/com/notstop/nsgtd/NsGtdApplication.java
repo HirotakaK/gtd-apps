28ca5be7609eaeb0b2d497f49a13a1c63acb5bb5
HirotakaKikushima <caealeajactaest@email.com>
2019-04-30 00:22:01 +0900

    不要ソースを削除

--- src/main/java/com/notstop/nsgtd/NsGtdApplication.java
+++ src/main/java/com/notstop/nsgtd/NsGtdApplication.java
@@ -1,18 +1,7 @@
 package com.notstop.nsgtd;
 
 import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.web.bind.annotation.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-//@SpringBootApplication
-//public class NsGtdApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(NsGtdApplication.class, args);
-//	}
-//
-//}
 
 @SpringBootApplication
 public class NsGtdApplication {
