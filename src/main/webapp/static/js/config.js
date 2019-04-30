573b1cc8b73dea1e1da5af9a7a8af238c82b86f6
HirotakaKikushima <caealeajactaest@email.com>
2019-04-30 23:57:10 +0900

    ここを参考にしてログイン+リダイレクトの実装+GAE確認済 「[Firebase] Authenticationでメール認証 (Web編) その1」https://blog.katsubemakito.net/firebase/firebase-authentication-email-web1

---
+++ src/main/webapp/static/js/config.js
@@ -0,0 +1,10 @@
+// Firebaseの初期化
+var config = {
+		apiKey : "AIzaSyCLbKtqW74zI0QNHbteqzIRPfxWsVwBxfs",
+		authDomain : "mygcpproject-146511.firebaseapp.com",
+		databaseURL : "https://mygcpproject-146511.firebaseio.com",
+		projectId : "mygcpproject-146511",
+		storageBucket : "mygcpproject-146511.appspot.com",
+		messagingSenderId : "768267993900"
+};
+firebase.initializeApp(config);
